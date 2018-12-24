2d7e6c34cc3b76ca6772187d8cab5f26804c39df
Misha <ostinpower03@gmail.com>
2018-12-24 14:21:58 +0500

    с аккордеоном печаль

--- source/js/main.js
+++ source/js/main.js
@@ -12,23 +12,5 @@ $('.about-slider').slick({
   slidesToScroll: 1
 });
 
-window.onload = function() {
-	var btn = document.querySelectorAll('.drop-down');
-	for(i=0; i < btn.length; i++) {
-		btn[i].addEventListener('click', function() {
-			
-			
-			if(this.nextElementSibling == 'block') {
-				this.nextElementSibling = 'none';
-				drop-down.innerHTML = '.list-action__name';
-			
-			}
+// Accordion
 
-			else {
-				this.nextElementSibling == 'block';
-				drop-down.innerHTML = '.list-action__name';
-			}
-		}
-		});
-	}
-};
