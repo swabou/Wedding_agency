7e8a6fd49e3f19b2dc18004fa350995ee344d27a
Misha <ostinpower03@gmail.com>
2018-12-24 14:04:56 +0500

    Ничего не вышло с JS

--- source/js/main.js
+++ source/js/main.js
@@ -11,3 +11,24 @@ $('.about-slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1
 });
+
+window.onload = function() {
+	var btn = document.querySelectorAll('.drop-down');
+	for(i=0; i < btn.length; i++) {
+		btn[i].addEventListener('click', function() {
+			
+			
+			if(this.nextElementSibling == 'block') {
+				this.nextElementSibling = 'none';
+				drop-down.innerHTML = '.list-action__name';
+			
+			}
+
+			else {
+				this.nextElementSibling == 'block';
+				drop-down.innerHTML = '.list-action__name';
+			}
+		}
+		});
+	}
+};
