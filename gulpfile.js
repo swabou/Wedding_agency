4c4c7a4718115e58189b2f5cf95b0457fd7d38b7
Misha <ostinpower03@gmail.com>
2019-01-21 18:53:39 +0500

    Плагины

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@ var server = require('browser-sync');
 var svgSprite = require('gulp-svg-sprite');
 var svgstore = require("gulp-svgstore");
 var rename = require('gulp-rename');
-var autoprefixer = require("autoprefixer");
+var autoprefixer = require("gulp-autoprefixer");
 var imagemin = require("gulp-imagemin");
 var cleancss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var csso = require('csso');
+var concatCss = require('gulp-concat-css');
 
 /*Task for Sass*/
 gulp.task('sass', function() { //Таск sass
@@ -36,6 +39,35 @@ gulp.task("spritesvg", function () {
   }))
     .pipe(rename("sprite-icon.svg"))
     .pipe(gulp.dest("source/img/sprite"));
+});
+
+
+
+
+
+// min css 
+gulp.task('cssmin', function () {
+    gulp.src('source/css/*.css')
+        .pipe(cleancss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/css'));
+});
+
+//Autopref
+gulp.task('autoprefixer', function () {
+    return gulp.src('source/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('source/css'));
+});
+
+//Image Compress
+gulp.task('compress', function() {
+  gulp.src('source/img/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('build/img'))
 });
 
 gulp.task('start', gulp.series('sass', 'server'));
